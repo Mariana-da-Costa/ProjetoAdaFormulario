50b3c57ce5cfc90742e6bbeac646714f4f420149
Mariana da Costa <marianacosta.engc@gmail.com>
2023-03-27 00:14:22 -0300

    Implementação do shared preferences e tela de login

--- app/src/main/java/com/ada_formulario/FormularioActivity.kt
+++ app/src/main/java/com/ada_formulario/FormularioActivity.kt
@@ -1,26 +1,62 @@
 package com.ada_formulario
 
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ada_formulario.databinding.ActivityFormularioBinding
 
 class FormularioActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityFormularioBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    @SuppressLint("CommitPrefEdits")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         viewBinding = ActivityFormularioBinding.inflate(layoutInflater)
         val view = viewBinding.root
         setContentView(view)
-        saveInformations()
+        recoverInformation()
+        saveInformation()
     }
 
-    private fun saveInformations() {
-        prefs.username = viewBinding.etUsername.text.toString()
-        prefs.email = viewBinding.etEmail.text.toString()
-        prefs.password = viewBinding.etPassword.text.toString().toInt()
-        prefs.age = viewBinding.etAge.text.toString()
-        prefs.cpf = viewBinding.etCpf.text.toString()
+    private fun recoverInformation() {
+        val preferences: SharedPreferences =
+            getSharedPreferences(KEEP_CONNECTED_PREFS, MODE_PRIVATE)
+
+        val username = preferences.getString(USERNAME, "")
+        val email = preferences.getString(EMAIL, "")
+        val password = preferences.getString(PASSWORD, "")
+        val age = preferences.getString(AGE, "")
+        val cpf = preferences.getString(CPF, "")
+
+        viewBinding.etUsername.hint = username
+        viewBinding.etEmail.hint = email
+        viewBinding.etPassword.hint = password
+        viewBinding.etAge.hint = age
+        viewBinding.etCpf.hint = cpf
+    }
+
+    private fun saveInformation() {
+        val preferences: SharedPreferences =
+            getSharedPreferences(KEEP_CONNECTED_PREFS, MODE_PRIVATE)
+
+        viewBinding.btnSave.setOnClickListener {
+            preferences.edit()
+                .putString(USERNAME, viewBinding.etUsername.text.toString())
+                .putString(EMAIL, viewBinding.etEmail.text.toString())
+                .putString(PASSWORD, viewBinding.etPassword.text.toString())
+                .putString(AGE, viewBinding.etAge.text.toString())
+                .putString(CPF, viewBinding.etCpf.text.toString())
+                .apply()
+        }
+    }
+
+    companion object {
+        const val USERNAME = "username"
+        const val EMAIL = "email"
+        const val PASSWORD = "password"
+        const val AGE = "age"
+        const val CPF = "cpf"
+        const val KEEP_CONNECTED_PREFS = "SAVE_LOGIN_PREFERENCES"
     }
 }
--- app/src/main/java/com/ada_formulario/MainActivity.kt
+++ app/src/main/java/com/ada_formulario/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.ada_formulario
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ada_formulario.FormularioActivity.Companion.EMAIL
+import com.ada_formulario.FormularioActivity.Companion.KEEP_CONNECTED_PREFS
+import com.ada_formulario.FormularioActivity.Companion.PASSWORD
+import com.ada_formulario.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = viewBinding.root
+        setContentView(view)
+
+        val preferences = getSharedPreferences(KEEP_CONNECTED_PREFS, MODE_PRIVATE)
+        val email = preferences.getString(EMAIL, "")
+        val password = preferences.getString(PASSWORD, "")
+
+        viewBinding.editTEmailMain.hint = email
+        viewBinding.editTPasswordMain.hint = password
+
+        viewBinding.btnLoginMain.setOnClickListener {
+            val intent = Intent(this, FormularioActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
